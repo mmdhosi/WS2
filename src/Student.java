f2a1f51d1e4031755cf8ed0fe085e56bf650c21e
mmdhosi <mmdhosi19@gmail.com>
2023-02-26 13:48:54 +0330

    added student class

---
+++ src/Student.java
@@ -0,0 +1,46 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String id;
+    private double grade;
+
+
+    public Student(String firstName, String lastName, String id, double grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.grade = grade;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+}
